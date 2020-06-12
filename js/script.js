fdafc242c6c0f8eb307ad9ff993a983d4c21efb5
Helicatic <39127187+Helicatic@users.noreply.github.com>
2020-06-12 22:15:16 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ var overlay = document.querySelector(".modal-overlay");
 var openMenu = document.querySelector(".header__toggle");
 var closeMenu = document.querySelector(".header__mobile-close");
 
+var loginBtn = document.querySelector(".login-btn");
+var loginModal = document.querySelector(".modal-login");
+var loginClose = document.querySelector(".login-close");
 
+var registerBtn = document.querySelector(".register-btn");
+var registerModal = document.querySelector(".modal-register");
+var registerClose = document.querySelector(".register-close");
+
+var loginBtnMob = document.querySelector(".login-btn-mob");
+var registerBtnMob = document.querySelector(".register-btn-mob");
+
+
+
+// menu
 openMenu.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.add("menu-show");
@@ -21,6 +34,60 @@ overlay.addEventListener("click", function (evt) {
   menu.classList.remove("menu-show");
   overlay.classList.remove("modal-show");
 });
+
+// login popup
+
+loginBtnMob.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginModal.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+});
+
+loginBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginModal.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginModal.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginModal.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+// register popup
+
+registerBtnMob.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  registerModal.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+});
+
+registerBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  registerModal.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+});
+
+registerClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  registerModal.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  registerModal.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+
 
 var header = document.querySelector('.header');
 window.onscroll = function showHeader() {
